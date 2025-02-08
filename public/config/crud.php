6d6b1eda8001dc61d6f68653af2d3d25ef7b6c95
joao-everton <jaoeversantos@gmail.com>
2025-02-07 22:09:21 -0300

    ultima do dia 07 02

--- public/config/crud.php
+++ public/config/crud.php
@@ -1,6 +1,25 @@
 <?php
     include "public/config/config.php";
     
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+
+    
+
+    $sql = "INSERT INTO usuarios (nome, email, telefone, senha) VALUES (DEFAULT, DEFAULT, '$nome', '$email', '$telefone', '$senha', DEFAULT)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Requisição de cadastro realizada com sucesso!";
+    } else {
+        echo "Erro: " . $conn->error;
+    }
+    
+    $conn->close();
+
+    
         
 
 ?>
--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,57 @@
-let isLogin = true;
-        document.getElementById('toggleButton').addEventListener('click', function() {
-            const formTitle = document.getElementById('formTitle');
-            const nameField = document.getElementById('nameField');
-            const submitButton = document.querySelector('button[type="submit"]');
-            
-            if (isLogin) {
-                formTitle.textContent = "Registro";
-                nameField.classList.remove('hidden');
-                telefone.classList.remove('hidden');
-                submitButton.textContent = "Registrar";
-                this.textContent = "Já tem uma conta? Faça login";
-            } else {
-                formTitle.textContent = "Login";
-                nameField.classList.add('hidden');
-                telefone.classList.add('hidden');
-                submitButton.textContent = "Entrar";
-                this.textContent = "Criar uma conta";
-            }
-            isLogin = !isLogin;
-        });
+let isLogin = false;
+    document.getElementById('toggleButton').addEventListener('click', function() {
+        const formTitle = document.getElementById('formTitle');
+        const nameField = document.getElementById('nameField');
+        const email = document.getElementById('email');
+        const submitButton = document.querySelector('button[type="submit"]');
+        
+        if (isLogin) {
+            formTitle.textContent = "Login";
+            nameField.classList.add('hidden');
+            email.placeholder = "Email / Nome";
+            telefone.classList.add('hidden');
+            submitButton.textContent = "Entrar";
+            this.textContent = "Criar uma conta";
+
+        } else {
+            formTitle.textContent = "Registro";
+            email.placeholder = "Email";
+            nameField.classList.remove('hidden');
+            telefone.classList.remove('hidden');
+            submitButton.textContent = "Registrar";
+            this.textContent = "Já tem uma conta? Faça login";
+        }
+        isLogin = !isLogin;
+    });
+
+    if (isLogin) {
+        function cadastrar() {
+        const nome = document.getElementById('formTitle').value;
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "public/config/crud.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onload = function () {
+            alert(xhr.responseText);
+        };
+        xhr.send(`nome=${nome}&email=${email}&senha=${senha}`);
+        }
+    }
+    else { 
+        function login() {
+            const email = document.getElementById('emailLogin').value;
+            const senha = document.getElementById('senhaLogin').value;
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", "public/config/crud.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onload = function () {
+                alert(xhr.responseText);
+            };
+            xhr.send(`email=${email}&senha=${senha}`);
+        }
+
+    }
+
