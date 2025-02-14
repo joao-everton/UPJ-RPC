0f7e5fef39a8bb50d3722bfb9a68e928055d0b54
joao-everton <jaoeversantos@gmail.com>
2025-02-13 23:49:08 -0300

    23:49 13 02

--- public/js/login.js
+++ public/js/login.js
@@ -27,7 +27,7 @@ let isLogin = true;
     });
 
         if (!isLogin) { 
-            document.getElementById('Form').addEventListener('submit', function(e) {
+            document.getElementById('submitbtn').addEventListener('click', function(e) {
                 e.preventDefault();  // Evitar o envio normal do formulário
         
                 const nome = document.getElementById('nameField').value;
