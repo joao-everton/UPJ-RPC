98912b82a60f581dc1cc2d76e993e671c2f3ee3b
joao-everton <jaoeversantos@gmail.com>
2025-02-15 18:17:34 -0300

    18:17 15 02

--- public/config/crud.php
+++ public/config/crud.php
@@ -10,15 +10,15 @@ function cadastro($nome, $email, $telefone, $senha, $conn) {
         $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
+        $stmt->store_result();
         if ($stmt->num_rows > 0) {
             return json_encode(["success" => false, "error" => "E-mail já cadastrado"]);
         }
-        else {
         $stmt = $conn->prepare("INSERT INTO usuarios (nome, email, telefone, senha) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("ssis", $nome, $email, $telefone, $senhaHash);
         $stmt->execute();
         return json_encode(["success" => true]);
-        }
+        
     } catch (Throwable $e) {
         return json_encode(["success" => false, "error" => $e->getMessage()]);
     }
