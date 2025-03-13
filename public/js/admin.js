e2374021d21d9af0632d8b33d7cd198ef31e835f
joao-everton <jaoeversantos@gmail.com>
2025-03-13 10:42:05 -0300

    13 03 10:42

--- public/js/admin.js
+++ public/js/admin.js
@@ -25,13 +25,9 @@ async function buscarUsuariosPendentes() {
                 <td class="px-4 py-2">${usuario.nome}</td>
                 <td class="px-4 py-2">${usuario.email}</td>
                 <td class="px-4 py-2">${usuario.telefone}</td>
-                <td class="px-4 py-2">
-                    <button id="aprovarbtn" class="bg-green-500 text-white px-2 py-1 rounded">
-                        Aprovar
-                    </button>
-                    <button id="reprovarbtn" class="bg-red-500 text-white px-2 py-1 rounded">
-                        Rejeitar
-                    </button>
+                <td>
+                    <button onclick="atualizarStatus(${usuario.id_usuario}, 'ativo')">Aprovar</button>
+                    <button onclick="atualizarStatus(${usuario.id_usuario}, 'inativo')">Rejeitar</button>
                 </td>
             `;
             tbody.appendChild(row);
